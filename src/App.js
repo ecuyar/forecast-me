79e2fe2274c5f11fae251753e39d2e6ca2cb0771
Enes Can UYAR <uyar.enescan@gmail.com>
2022-07-15 19:15:56 +0300

    15.07.2022

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavigationBar from "./NavigationBar";
+import HomePage from "./pages/HomePage";
+import CityPage from "./pages/CityPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<NavigationBar />}>
+            <Route index exact path="" element={<HomePage />} />
+            <Route exact path="city" element={<CityPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
